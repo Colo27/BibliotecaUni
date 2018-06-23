31ffc5292456e5cc2b806c81b5876c6473012ba1
Unknown <colosplash27@gmail.com>
2018-06-23 11:32:50 +0200

    I file del progetto della biblioteca di Gallo

---
+++ app/src/main/java/com/example/guest/gestionebiblioteca/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.guest.gestionebiblioteca;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        CardView cerca = (CardView) findViewById(R.id.cerca_layout);
+        CardView account = (CardView) findViewById(R.id.account_layout);
+
+        cerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cerca = new Intent(getBaseContext(), Cerca.class);
+                startActivity(cerca);
+            }
+        });
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent account = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(account);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/statoprestiti.java
@@ -0,0 +1,36 @@
+package com;
+
+/**
+ * Created by elisaboninella on 20/06/18.
+ */
+
+public class statoprestiti {
+    public class DemotimerActivity extends Activity {
+        /** Called when the activity is first created. */
+        TextView tv;
+        long diff;
+        long oldLong;
+        long NewLong;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            tv = new TextView(this);
+            this.setContentView(tv);
+            SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy, HH:mm");
+            String oldTime = "19.02.2018, 12:00";//Timer date 1
+            String NewTime = "20.02.2018, 14:00";//Timer date 2
+            Date oldDate, newDate;
+            try {
+                oldDate = formatter.parse(oldTime);
+                newDate = formatter.parse(NewTime);
+                oldLong = oldDate.getTime();
+                NewLong = newDate.getTime();
+                diff = NewLong - oldLong;
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            MyCount counter = new MyCount(diff, 1000);
+            counter.start();
+        }
+
+    }
