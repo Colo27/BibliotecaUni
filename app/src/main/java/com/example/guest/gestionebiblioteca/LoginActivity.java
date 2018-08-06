c7fedfe597e45629f9a50bcce48d71440155a67e
Unknown <colosplash27@gmail.com>
2018-08-06 11:38:34 +0200

    Modifiche

--- app/src/main/java/com/example/guest/gestionebiblioteca/LoginActivity.java
+++ app/src/main/java/com/example/guest/gestionebiblioteca/LoginActivity.java
@@ -35,6 +35,7 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
+
 /**
  * A login screen that offers login via email/password.
  */
@@ -62,7 +63,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/guest/gestionebiblioteca/PrenotaActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.gestionebiblioteca;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PrenotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
