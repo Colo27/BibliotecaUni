82ebc0c73caff640351eeb82e42d023ceba64b3d
Unknown <elisa.bonni95@libero.it>
2018-08-16 18:07:29 +0200

    fix name

--- app/src/main/java/com/example/guest/gestionebiblioteca/AccountActivity.java
+++ app/src/main/java/com/example/guest/gestionebiblioteca/AccountActivity.java
@@ -6,12 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        TextView name = findViewById(R.id.textView3);
+
+        name.setText(user.getDisplayName());
 
         TextView informazioni = findViewById(R.id.textView5);
 
