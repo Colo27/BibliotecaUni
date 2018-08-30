0996c9c22056b2985750d74982eee5c3cadcc284
Marco <ffmarco96@gmail.com>
2018-08-31 00:29:56 +0200

    mancano ultimi ritocchi

--- app/src/main/java/com/example/guest/gestionebiblioteca/RestituisciActivity.java
+++ app/src/main/java/com/example/guest/gestionebiblioteca/RestituisciActivity.java
@@ -1,5 +1,6 @@
 package com.example.guest.gestionebiblioteca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,11 +21,13 @@ public class RestituisciActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private String mUserId;
-    private String user;
     private String mAutore;
     private String mTitolo;
     private String mKey;
 
+
+    private TextView textTitolo;
+    private TextView textAutore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +35,37 @@ public class RestituisciActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restituisci);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        mUser=mAuth.getCurrentUser();
-        mUserId = mUser.getUid();
 
+        textTitolo = findViewById(R.id.edTitolo);
+        textAutore = findViewById(R.id.edAutore);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mUserId = mUser.getUid();
 
         mAutore = getIntent().getStringExtra("autore");
         mTitolo = getIntent().getStringExtra("titolo");
         mKey = getIntent().getStringExtra("key");
+
+        textTitolo.setText(mTitolo);
+        textAutore.setText(mAutore);
 
         final Button restituisci = (Button) findViewById(R.id.btnRestituisciLibro);
         restituisci.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                mDatabaseReference.child("users").child("admin").push().setValue(restituisci);
+                Libro restituzione = new Libro(mAutore, mTitolo);
 
-
+                mDatabaseReference.child("users").child("admin").push().setValue(restituzione);
                 mDatabaseReference.child("users").child(mUserId).child(mKey).removeValue();
 
+                Toast.makeText(RestituisciActivity.this, "Restituzione effettuata con successo", Toast.LENGTH_LONG).show();
 
+                Intent intent = new Intent(RestituisciActivity.this, MainActivity.class);
+                intent.putExtra("user", "user");
+                startActivity(intent);
             }
         });
 
